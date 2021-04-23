83188f6a74806d8db5a905027bb53086ef09b4cb
BEUSER <beuser@BNE309-SALMACLT.local>
2021-04-23 07:46:14 -0400

    Update Calculate True ROP

--- CalcTruROP.R
+++ CalcTruROP.R
@@ -13,8 +13,32 @@ sU<-P2*(b-a)+a-(Q/2)
 # The error function
 erf <- function(x) 2 * pnorm(x * sqrt(2)) - 1
 
-#Truncated Normal
+#Truncated Normal all P2 > 0.9
 library(truncnorm)
+
+P2<-0.80
+Q<-19
+mu<-100
+sig<-20
+
+#Truncated Normal Expected Shorts
+TNES<-function(z)
+  {
+   ((mu-z)*sqrt(pi/2)+sig*exp(-(1/2)*((z-mu)/sig)^2)+
+      sig*sqrt(pi/2)*((z-mu)/sig)*erf((z-mu)/(sig*sqrt(2))))/
+    (sqrt(2*pi)*(1-0.5*(1+erf(-mu/(sig*sqrt(2))))))
+  }
+
+sTN1<-optimize(function(z){abs(Q*(1-P2)-(((mu-z)*sqrt(pi/2)+sig*exp(-(1/2)*((z-mu)/sig)^2)+
+                                    sig*sqrt(pi/2)*((z-mu)/sig)*erf((z-mu)/(sig*sqrt(2))))/
+                                    (sqrt(2*pi)*(1-0.5*(1+erf(-mu/
+                                    (sig*sqrt(2))))))))},lower=0,upper=(mu+6*sig))$minimum
+
+sN1<-optimize(function(z){abs(((Q/sig)*(1-P2))-((dnorm(z)-
+                                    z*(1-pnorm(z)))))},lower = 0,upper = 6)$minimum*sig+mu
+curve(TNES(x),from = 0,to=mu+6*sig)
+
+#Truncated Normal all P2 no double counting
 
 #tn<-rtruncnorm(10^6,0,Inf,100,20)
 #mean(tn)
@@ -41,7 +65,7 @@ VQ<-function(z){(1/(2*sqrt(pi)*(1-(1/2)*(1+erf(-mu/(sig*sqrt(2)))))))*
                                         (Q+z-mu)*erf((Q+z-mu)/(sig*sqrt(2)))+
                                         Q*erf(mu/(sig*sqrt(2)))))}
 
-sTN<-optimize(function(z){abs(P2-
+sTN2<-optimize(function(z){abs(P2-
       ((1/(2*sqrt(pi)*(1-(1/2)*(1+erf(-mu/(sig*sqrt(2)))))))*
       (sig*sqrt(2)*(exp(-(Q+z-mu)^2/(2*sig^2))-exp(-(z-mu)^2/(2*sig^2)))+
       sqrt(pi)*((mu-z)*erf((z-mu)/(sig*sqrt(2)))+
@@ -52,7 +76,7 @@ sTN<-optimize(function(z){abs(P2-
 
 #curve(VQ(x),from = 0,to = 300)
 
-#sN<-optimize(function(z){abs(((Q/sig)*(1-P2))-((dnorm(z)-z*(1-pnorm(z)))-
+#sN2<-optimize(function(z){abs(((Q/sig)*(1-P2))-((dnorm(z)-z*(1-pnorm(z)))-
 #                        (dnorm(z+Q/sig)-(z+Q/sig)*(1-pnorm(z+Q/sig)))))},
 #             lower = 0,upper = 6)$minimum*sig+mu
 
@@ -61,7 +85,7 @@ sTN<-optimize(function(z){abs(P2-
 
 # curve(normalLoss(x),from = -6,to=6)
 
-# Lognormal
+# Lognormal All P2 no double counting
 
 curve(dlnorm(x,4.585559829,0.1980422),from = 0,to=300)
 
@@ -79,3 +103,33 @@ sLN<-optimize(function(z){abs(P2-
                                erf((mu+sig^2-log(z+Q))/(sig*sqrt(2)))))/
             Q)},lower=0,upper=(100+6*CV*100))$minimum
 
+
+# Gamma distribution All P2 no double counting
+
+curve(dlnorm(x,4.585559829,0.1980422),from = 0,to=300)
+
+P2<-0.8
+Q<-19
+mu<-100
+sig<-20
+alpha<-mu^2/sig^2
+beta<-mu/sig^2
+CV<-0.2
+
+EVQGam<-function(z){Q*pgamma(z,alpha,beta)+(z+Q)*
+    (pgamma(z+Q,alpha,beta)-pgamma(z,alpha,beta))-(alpha/beta)*(pgamma(z+Q,alpha+1,beta)-
+                                                                pgamma(z,alpha+1,beta))}
+
+curve(EVQGam(x),from = 0,to=mu+6*sig)
+curve(dgamma(x,alpha,beta),from = 0,to=mu+6*sig)
+curve(dnorm(x,mu,sig),from = 0,to=mu+6*sig)
+
+sGam<-optimize(function(z){abs(P2-
+                                (Q*pgamma(z,alpha,beta)+(z+Q)*(pgamma(z+Q,alpha,beta)-
+                                pgamma(z,alpha,beta))-(alpha/beta)*(pgamma(z+Q,alpha+1,beta)-
+                                pgamma(z,alpha+1,beta)))/
+                                 Q)},lower=0,upper=(100+6*CV*100))$minimum
+
+sN2<-optimize(function(z){abs(((Q/sig)*(1-P2))-((dnorm(z)-z*(1-pnorm(z)))-
+                        (dnorm(z+Q/sig)-(z+Q/sig)*(1-pnorm(z+Q/sig)))))},
+              lower = 0,upper = 6)$minimum*sig+mu
