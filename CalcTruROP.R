23f4b5420c306a8e82b1c52aab8f339ffd0de743
BEUSER <beuser@BNE309-SALMACLT.local>
2021-04-27 13:33:41 -0400

    Update files.

--- CalcTruROP.R
+++ CalcTruROP.R
@@ -1,237 +1,110 @@
-# Calculate the True ROP FPR  0.9 < P_2 <= 0.9
+# Calculate the True ROP FOR  0.9 < P_2 <= 0.9 Using Mathematica Generated Analytical Formula
+#   File updated April 27, 2021 without the bimodal True ROP expressions
+#   For details and other supporting results see the FRBootstrapNotes.RMD markdown file
 
 # The error function
 erf <- function(x) 2 * pnorm(x * sqrt(2)) - 1
 
+#INITIALIZE INPUTS
+P2<-0.80 #Fill rate target
+P2lvls<-6 #no. of levels of P2
+Q<-19 #Fixed order quantity
+Dist<-11 #no. of distributions (NOT INCLUDING BIMODAL)
+#read in distribution parameter and other experimental inputs
+ExpInputs<-read.csv("ExpInputs.csv",header = FALSE)
+Qexps<-read.csv("QExps.csv",header = FALSE)
+U<-cbind(c(1:6),c(0.8,0.85,0.9,0.95,0.98,0.99))
 
-# UNIFORM DISTRIBUTION
+# OUTPUT
+TruROP<-matrix(0,nrow=11,ncol=6) # TRUE ROP TRADITIONAL EXP SHORTS FUNCTION
+SMTruROP<-matrix(0,nrow=11,ncol=6) # TRUE ROP Silver (1970) MODIFIED FUNCTION
 
-# Case 1: P2 > 0.9
-P2<-0.98
-Q<-94
-mu<-100
-sig<-20
-a<-66
-b<-134
+#Distributional inputs
+for (d in 1:11){ # Cycle through the distributions in ExpInputs (NOT INCLUDING BIMODAL)
 
-sU1<-Q*(1-P2)*(b-a)+b
+  # Identifies the CV of the distribution to select correct Q
+  if (d==1|d==4|d==7|d==12|d==17|d==20) {
+    CV<-1
+    cv<-1/5
+  } else if (d==2|d==5|d==8|d==13|d==18|d==21) {
+    CV<-2
+    cv<-1/3
+  } else if (d==3|d==6|d==9|d==14|d==19|d==22) {
+    CV<-3
+    cv<-1/2
+  } else if (d==10|d==15) {
+    CV<-4
+    cv<-1
+  } else {
+    CV<-5
+    cv<-2
+  }
+  
+  for (c in U[,1]) {
+    
+    P2<-U[c,2] # The P2 experimental level
+    Q<-Qexps[c,CV] # Use the correct Q
+  
+  if (d<=3){
+    # UNIFORM DISTRIBUTION
+    a<-ExpInputs[d,1]
+    b<-ExpInputs[d,2]
+    # Case 1: P2 > 0.9
+    
+    TruROP[d,c]<-Q*(1-P2)*(b-a)+b
+    
+    # Case 2: P2 <= 0.9
+    
+    SMTruROP[d,c]<-P2*(b-a)+a-(Q/2)
+    }
+    
+    if (d>=4 && d<=6){
+    # TRUNCATED NORMAL DISTRIBUTION
+    mu<-ExpInputs[d,1]
+    sig<-ExpInputs[d,2]
+    # Case 1: P2 > 0.9
+    
+    TruROP[d,c]<-optimize(function(z){abs(Q*(1-P2)-(((mu-z)*sqrt(pi/2)+sig*exp(-(1/2)*((z-mu)/
+                  sig)^2)+sig*sqrt(pi/2)*((z-mu)/sig)*erf((z-mu)/(sig*sqrt(2))))/
+                  (sqrt(2*pi)*(1-0.5*(1+erf(-mu/
+                  (sig*sqrt(2))))))))},lower=0,upper=(mu+6*sig))$minimum
+    
+    # Case 2: P2 <= 0.9
+    
+    SMTruROP[d,c]<-optimize(function(z){abs(P2-
+          ((1/(2*sqrt(pi)*(1-(1/2)*(1+erf(-mu/(sig*sqrt(2)))))))*
+          (sig*sqrt(2)*(exp(-(Q+z-mu)^2/(2*sig^2))-exp(-(z-mu)^2/(2*sig^2)))+
+          sqrt(pi)*((mu-z)*erf((z-mu)/(sig*sqrt(2)))+
+          (Q+z-mu)*erf((Q+z-mu)/(sig*sqrt(2)))+Q*erf(mu/(sig*sqrt(2))))))/Q)},
+          lower=0,upper=(mu+6*sig))$minimum
+    }
+    
+    if (d>=7 && d<=11){
+    # LOGNORMAL DISTRIBUTION
+    mu<-ExpInputs[d,1]
+    sig<-ExpInputs[d,2]
+    # Case 1: > 0.9
+    
+    TruROP[d,c]<-optimize(function(z){abs(abs(Q*(1-P2))-(1/2*(exp(mu+sig^2/2)-z-z*
+          erf((mu-log(z))/(sig*sqrt(2)))+exp(mu+sig^2/2)*erf((mu+sig^2-log(z))/
+          (sig*sqrt(2))))))},lower=0,upper=(100+6*cv*100))$minimum
+    
+    # Case 2: P2 <= 0.9
+    
+    SMTruROP[d,c]<-optimize(function(z){abs(P2-
+        (Q/2*(1-erf((mu-log(z))/(sig*sqrt(2))))+(z+Q)/2*(erf((mu-log(z))/
+        (sig*sqrt(2)))-erf((mu-log(z+Q))/(sig*sqrt(2))))-exp(mu+sig^2/2)/2*
+        (erf((mu+sig^2-log(z))/(sig*sqrt(2)))-erf((mu+sig^2-log(z+Q))/(sig*sqrt(2)))))/Q)},
+        lower=0,upper=(100+6*cv*100))$minimum
+    }
 
-# Case 2: P2 <= 0.9
-P2<-0.8
-Q<-19
-mu<-100
-sig<-20
-a<-66
-b<-134
+    # Bimodal Distribution # 
+    
+    # *********  TO BE COMPLETED  *********
 
-sU2<-P2*(b-a)+a-(Q/2)
-
-# TRUNCATED NORMAL DISTRIBUTION
-library(truncnorm)
-#curve(dnorm(x,100,20),from = 0,to=300)
-
-# Case 1: P2 > 0.9
-
-P2<-0.80
-Q<-19
-mu<-100
-sig<-20
-
-#Truncated Normal Expected Shorts
-TNES<-function(z)
-{
-  ((mu-z)*sqrt(pi/2)+sig*exp(-(1/2)*((z-mu)/sig)^2)+
-     sig*sqrt(pi/2)*((z-mu)/sig)*erf((z-mu)/(sig*sqrt(2))))/
-    (sqrt(2*pi)*(1-0.5*(1+erf(-mu/(sig*sqrt(2))))))
+  }
 }
 
-sTN1<-optimize(function(z){abs(Q*(1-P2)-(((mu-z)*sqrt(pi/2)+sig*exp(-(1/2)*((z-mu)/sig)^2)+
-                                      sig*sqrt(pi/2)*((z-mu)/sig)*erf((z-mu)/(sig*sqrt(2))))/
-                                      (sqrt(2*pi)*(1-0.5*(1+erf(-mu/
-                                      (sig*sqrt(2))))))))},lower=0,upper=(mu+6*sig))$minimum
-
-#sN1<-optimize(function(z){abs(((Q/sig)*(1-P2))-((dnorm(z)-
-#                                z*(1-pnorm(z)))))},lower = 0,upper = 6)$minimum*sig+mu
-curve(TNES(x),from = 0,to=mu+6*sig)
-
-# Case 2: P2 <= 0.9
-
-P2<-0.85
-Q<-25
-mu<-100
-sig<-20
-
-TNVQ<-function(z){(1/(2*sqrt(pi)*(1-(1/2)*(1+erf(-mu/(sig*sqrt(2)))))))*
-                         (sig*sqrt(2)*(exp(-(Q+z-mu)^2/(2*sig^2))-
-                                         exp(-(z-mu)^2/(2*sig^2)))+
-                            sqrt(pi)*((mu-z)*erf((z-mu)/(sig*sqrt(2)))+
-                                        (Q+z-mu)*erf((Q+z-mu)/(sig*sqrt(2)))+
-                                        Q*erf(mu/(sig*sqrt(2)))))}
-
-sTN2<-optimize(function(z){abs(P2-
-      ((1/(2*sqrt(pi)*(1-(1/2)*(1+erf(-mu/(sig*sqrt(2)))))))*
-      (sig*sqrt(2)*(exp(-(Q+z-mu)^2/(2*sig^2))-exp(-(z-mu)^2/(2*sig^2)))+
-      sqrt(pi)*((mu-z)*erf((z-mu)/(sig*sqrt(2)))+
-                  (Q+z-mu)*erf((Q+z-mu)/(sig*sqrt(2)))+
-                  Q*erf(mu/(sig*sqrt(2))))))/
-      Q)},lower=0,upper=(mu+6*sig))$minimum
-
-
-curve(VQ(x),from = 0,to = 300)
-
-#sN2<-optimize(function(z){abs(((Q/sig)*(1-P2))-((dnorm(z)-z*(1-pnorm(z)))-
-#                        (dnorm(z+Q/sig)-(z+Q/sig)*(1-pnorm(z+Q/sig)))))},
-#             lower = 0,upper = 6)$minimum*sig+mu
-
-# LOGNORMAL DISTRIBUTION
-# The comparison of the lognormal and normal graphs below shows the marginal difference in s
-#   for expected shorts calculated by the lognormal and normal distributions below
-curve(dlnorm(x,4.585559829,0.1980422),from = 0,to=300)
-curve(dnorm(x,100,20),from = 0,to=300)
-
-# Case 1: > 0.9
-
-P2<-0.95
-Q<-44
-mu<-4.585559829
-sig<-0.1980422
-CV<-0.2
-
-#Truncated Normal Expected Shorts
-LnES<-function(z)
-{
-  1/2*(exp(mu+sig^2/2)-z-z*erf((mu-log(z))/(sig*sqrt(2)))+
-         exp(mu+sig^2/2)*erf((mu+sig^2-log(z))/(sig*sqrt(2))))
-}
-
-sLN1<-optimize(function(z){abs(abs(Q*(1-P2))-
-                                 (1/2*(exp(mu+sig^2/2)-z-z*erf((mu-log(z))/(sig*sqrt(2)))+
-                                 exp(mu+sig^2/2)*erf((mu+sig^2-log(z))/
-                                 (sig*sqrt(2))))))},lower=0,upper=(100+6*CV*100))$minimum
-
-curve(LnES(x),from = 0,to=100+6*20)
-
-muX<-100
-sigX<-20
-sN1<-optimize(function(z){abs(((Q/sigX)*(1-P2))-((dnorm(z)-
-                                    z*(1-pnorm(z)))))},lower = 0,upper = 6)$minimum*sigX+muX
-
-# Case 2: P2 <= 0.9
-P2<-0.8
-Q<-19
-mu<-4.585559829
-sig<-0.1980422
-CV<-0.2
-
-
-
-sLN2<-optimize(function(z){abs(P2-
-          (Q/2*(1-erf((mu-log(z))/(sig*sqrt(2))))+
-          (z+Q)/2*(erf((mu-log(z))/(sig*sqrt(2)))-erf((mu-log(z+Q))/
-                                                         (sig*sqrt(2))))-
-          exp(mu+sig^2/2)/2*(erf((mu+sig^2-log(z))/(sig*sqrt(2)))-
-                               erf((mu+sig^2-log(z+Q))/(sig*sqrt(2)))))/
-            Q)},lower=0,upper=(100+6*CV*100))$minimum
-
-muX<-100
-sigX<-20
-sN1<-optimize(function(z){abs(((Q/sigX)*(1-P2))-((dnorm(z)-z*(1-pnorm(z)))-
-                        (dnorm(z+Q/sigX)-(z+Q/sigX)*(1-pnorm(z+Q/sigX)))))},
-                        lower = 0,upper = 6)$minimum*sigX+muX
-
-# Bimodal Distribution
-
-# Case 1: P2 > 0.9
-P2<-0.95
-Q<-44
-mu1<-85.9
-sig1<-8.6
-mu2<-121.1
-sig2<-12.1
-p<-0.6
-q<-0.4 # q = 1-p
-CV<-0.2
-
-BMES<-function(z){p*z/2*(erf((z-mu1)/(sig1*sqrt(2)))-1)-mu1*p/2*(erf((z-mu1)/(sig1*sqrt(2)))-1)+
-    p/sqrt(2*pi)*sig1*exp(-(z-mu1)^2/(2*sig1^2))+
-    q*z/2*(erf((z-mu2)/(sig2*sqrt(2)))-1)-mu2*q/2*(erf((z-mu2)/(sig2*sqrt(2)))-1)+
-    q/sqrt(2*pi)*sig2*exp(-(z-mu2)^2/(2*sig2^2))}
-
-curve(BMES(x),from = 0,to=100+6*20)
-
-sBM1<-optimize(function(z)
-  {abs(abs(Q*(1-P2))-(p*z/2*(erf((z-mu1)/(sig1*sqrt(2)))-1)-mu1*p/2*(erf((z-mu1)/(sig1*sqrt(2)))-1)+
-        p/sqrt(2*pi)*sig1*exp(-(z-mu1)^2/(2*sig1^2))+
-        q*z/2*(erf((z-mu2)/(sig2*sqrt(2)))-1)-mu2*q/2*(erf((z-mu2)/(sig2*sqrt(2)))-1)+
-        q/sqrt(2*pi)*sig2*exp(-(z-mu2)^2/(2*sig2^2))))},
-  lower=0,upper=(100+6*CV*100))$minimum
-
-
-# Case 2: P2 <= 0.9
-
-P2<-0.8
-Q<-19
-mu1<-85.9
-sig1<-8.6
-mu2<-121.1
-sig2<-12.1
-p<-0.6
-q<-0.4 # q = 1-p
-CV<-0.2
-
-BMVQ<-function(z)
-  {1/sqrt(2*pi)*((z+Q)*sqrt(pi/2)*(p*erf((z+Q-mu1)/(sig1*sqrt(2)))-
-   p*erf((z-mu1)/(sig1*sqrt(2)))+(p-1)*(erf((z-mu2)/(sig2*sqrt(2)))-erf((Q+z-mu2)/
-   (sig2*sqrt(2)))))+Q*sqrt(pi/2)*(p*erf((z-mu1)/(sig1*sqrt(2)))+p*erf(mu1/(sig1*sqrt(2)))-
-   (p-1)*(erf((z-mu2)/(sig2*sqrt(2)))+erf(mu2/(sig2*sqrt(2)))))+mu1*p*sqrt(pi/2)*
-   (erf((z-mu1)/(sig1*sqrt(2)))-erf((z+Q-mu1)/(sig1*sqrt(2))))+mu2*(p-1)*sqrt(pi/2)*
-   (erf((Q+z-mu2)/(sig2*sqrt(2)))-erf((z-mu2)/(sig2*sqrt(2))))+
-   p*sig1*(exp(-(z+Q-mu1)^2/(2*sig1^2))-exp(-(z-mu1)^2/(2*sig1^2)))+
-  (sig2-p*sig2)*exp(-(z+Q-mu2)^2/(2*sig2^2))+(p*sig2*-sig2)*exp(-(z-mu2)^2/(2*sig2^2)))}
-
-curve(BMVQ(x),from=0, to=100+6*100*CV)
-
-sBM2<-optimize(function(z)
-  {abs(P2-(1/sqrt(2*pi)*(sqrt(pi/2)*(p*(z+Q)*erf((z+Q-mu1)/(sig1*sqrt(2)))-
-  p*z*erf((z-mu1)/(sig1*sqrt(2)))+p*Q*erf(mu1/(sig1*sqrt(2)))+(p*z-z)*
-  erf((z-mu2)/(sig2*sqrt(2)))+(Q-p*Q+z-p*z)*erf((z+Q-mu2)/(sig2*sqrt(2)))-
-  (p-1)*Q*erf(mu2/(sig2*sqrt(2))))+mu1*p*sqrt(pi/2)*(erf((z-mu1)/(sig1*sqrt(2)))-
-  erf((z+Q-mu1)/(sig1*sqrt(2))))+mu2*(p-1)*sqrt(pi/2)*(erf((Q+z-mu2)/(sig2*sqrt(2)))-
-  erf((z-mu2)/(sig2*sqrt(2))))+p*sig1*(exp(-(z+Q-mu1)^2/(2*sig1^2))-
-  exp(-(z-mu1)^2/(2*sig1^2)))+(sig2-p*sig2)*exp(-(z+Q-mu2)^2/(2*sig2^2))+
-  (p*sig2*-sig2)*exp(-(z-mu2)^2/(2*sig2^2))))/Q)},
-  lower=0,upper=(100+6*CV*100))$minimum
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-# Gamma distribution All P2 no double counting To be used in calculating the Silver (1970)
-#   modification for P2<= 0.9.
-
-#curve(dlnorm(x,4.585559829,0.1980422),from = 0,to=300)
-
-P2<-0.8
-Q<-19
-mu<-100
-sig<-20
-alpha<-mu^2/sig^2
-beta<-mu/sig^2
-CV<-0.2
-
-EVQGam<-function(z){Q*pgamma(z,alpha,beta)+(z+Q)*
-    (pgamma(z+Q,alpha,beta)-pgamma(z,alpha,beta))-(alpha/beta)*(pgamma(z+Q,alpha+1,beta)-
-                                                                pgamma(z,alpha+1,beta))}
-
-#curve(EVQGam(x),from = 0,to=mu+6*sig)
-#curve(dgamma(x,alpha,beta),from = 0,to=mu+6*sig)
-#curve(dnorm(x,mu,sig),from = 0,to=mu+6*sig)
-
-sGam<-optimize(function(z){abs(P2-
-                                (Q*pgamma(z,alpha,beta)+(z+Q)*(pgamma(z+Q,alpha,beta)-
-                                pgamma(z,alpha,beta))-(alpha/beta)*(pgamma(z+Q,alpha+1,beta)-
-                                pgamma(z,alpha+1,beta)))/
-                                 Q)},lower=0,upper=(100+6*CV*100))$minimum
-
-#sN2<-optimize(function(z){abs(((Q/sig)*(1-P2))-((dnorm(z)-z*(1-pnorm(z)))-
-#                        (dnorm(z+Q/sig)-(z+Q/sig)*(1-pnorm(z+Q/sig)))))},
-#              lower = 0,upper = 6)$minimum*sig+mu
-
+# OUTPUT THE TRUE CALCULATED ROP
+write.table(TruROP,file = "TrueCalcROP.csv",sep=",",row.names = FALSE,col.names=FALSE)
+write.table(SMTruROP,file = "SMTrueCalcROP.csv",sep=",",row.names = FALSE,col.names=FALSE)
--- TempWorkSpace.R
+++ TempWorkSpace.R
@@ -1,93 +1,37 @@
-R<-100
-B<-500
-P2<-0.98
-Q<-46
+R<-5
+B<-10
+P2<-0.80
+Q<-19
+nX<-24
+ExpInputs<-read.csv("ExpInputs.csv",header = FALSE)
 
-nX<-30
-LTDsmpl<-`X-3-7`
-sboot30_98<-apply(LTDsmpl,2,FRBoot)
-hist(sboot30_98)
-abline(v = 182.57, col = "red", lwd=2, lty=2);
-mean(sboot30_98)
-sd(sboot30_98)
+set.seed(931971)
 
-esMat<-apply(BtSmplX,2,ExpShort,n=nX)
+LTDSmpl<-replicate(R,sort(runif(nX,ExpInputs[1,1],ExpInputs[1,2])))
+which((LTDSmpl[1,1]+Q-LTDSmpl[,1])>0)
+j<-max(which((LTDSmpl[24,1]+Q-LTDSmpl[,1])>0))
 
-TS<-Q*(1-P2)
-
-# Calculate this difference before the for loop to calculate the s_hat vector
-diff<-TS-esMat
-
-# Function to find the largest negative value
-maxneg<-function(vec)
-{
-  if(length(which(vec<0))){
-  out<-max(which(vec==max(vec[vec<0])))
-  }
-  else {out<-0}
-  return(out)
-}
-
-# Function to find the smallest positive value
-minpos<-function(vect)
-{
-  out<-min(which(vect==min(vect[vect>0])))
-  return(out)
-}
-# Vector of resample indices with smallest ES>TS
-lo<-apply(diff,2,maxneg)
-# Vector of resample indices with smallest ES<TS
-hi<-apply(diff,2,minpos)
-
-s_hat<-1:B # vector to store the estimates ROP for each bootstrap resample
-
-for (k in 1:B) # Iterate over all B resamples
-{
-  # 1. The first condition is if at least one of the resample values ES = TS
-  # 2. If all resample values are equal
-  if(length(which(diff[,k]==0))>0)
+modExpShort<-function(x,n)
   {
-    if (which(diff[, k] == 0)) browser()
-    s_hat[k]<-BtSmplX[which(diff[,k]==0),k]
+    modshort<-(1:n)*0
+    for (i in 1:(n-1))
+      {
+        j<-max(which((x[i]+Q-x)>0))
+        modshort[i]<-Q-Q*i/n-(sum(x[i]+Q-x[(i+1):j])/n)
+        if(modshort[i]<0) browser()
+      }
+  return(modshort)
   }
-  # 2. The second ondition is if all resample values are equal 
-  if(length(which(diff[,k]==TS))==nX)
-  {
-    s_hat[k]<-BtSmplX[1,k]
-  }
-  # 3. The third condition is when all the resample values ES<TS  
-  else if(length(which(diff[,k]<0))==0 && length(which(diff[,k]==TS))<nX)
-  {
-    # This calculates the index of the second smallest bootstrap resample
-    m0<-max(which(BtSmplX[,k]==min(BtSmplX[,k])))+1
-    s_hat[k]<-BtSmplX[1,k]-(((TS-esMat[1,k])*(BtSmplX[m0,k]-BtSmplX[1,k]))/
-                              (esMat[1,k]-esMat[m0,k]))
-  }
-  # 4. The fourth condition is when resample ES values are such that ES(i)<TS<ES(j)
-  else 
-  {
-    s_hat[k]<-BtSmplX[hi[k],k]-(((TS-esMat[hi[k],k])*(BtSmplX[hi[k],k]-BtSmplX[lo[k],k]))/
-                                  (esMat[lo[k],k]-esMat[hi[k],k]))
-  }
-  if (is.nan(s_hat[k])) browser() # used for tracing the origin of the NaN values
-}
 
-BtSmplX7<-BtSmplX
-diff7<-diff
-esMat7<-esMat
-s_hat7<-s_hat
+test<-rep(80,24)
 
-sdes7<-apply(esMat7,2,sd)
-sdes12<-apply(esMat12,2,sd)
-sdes7<-apply(esMat7,2,sd)
-sdes7<-apply(esMat7,2,sd)
+test1<-modExpShort(test,24)
 
-sdesMat<-cbind(sdes7,sdes12,sdes7,sdes70)
-               
-NTruSS<-optimize(function(z){abs(((dnorm(z)-z*(1-pnorm(z)))-(dnorm(z+Qratio)-
-                                                                    (z+Qratio)*(1-pnorm(z+Qratio))))-
-                                        normloss)},lower=0,upper = 6)$minimum*SigmaX
+length(which(test1<10^-6))
 
-BTruROP<-TruLTD[hi]-(((TS-ES[hi])*(TruLTD[hi]-TruLTD[lo]))/
-                            (ES[lo]-ES[hi]))
-               
+bimod<-bimodDistFunc(10^4,ExpInputs[12,1],ExpInputs[12,2],ExpInputs[12,3],
+                                   ExpInputs[12,4],ExpInputs[12,5])
+
+hist(bimod)
+bimod<-sort(bimod)
+modES<-modExpShort(bimod,10^4)
